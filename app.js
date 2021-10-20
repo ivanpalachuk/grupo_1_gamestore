4782605b6b1eef515bd45faa05792a913ac5ef58
ivanpalachuk <ivanzar@hotmail.com>
2021-10-20 20:05:07 -0300

    servidor a terminar, head con tipografia e icono agregado

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath))
+app.listen(4022, () => {
+    console.log("Servidor prueba Gamestore corriendo en puerto 4022")
+})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/shopping-cart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/shoppingcart.html"))
+})
