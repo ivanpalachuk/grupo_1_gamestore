fd02536e0813bc894732838b027b28d22cf37895
esteban18498 <esteban18498@gmail.com>
2021-11-04 19:47:42 -0300

    [add] EJS functions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+const { debug } = require("console");
 const express = require("express");
 const path = require("path");
 const app = express();
-const publicPath = path.resolve(__dirname, "./public");
+const publicPath = path.resolve(__dirname, "../public");
 const port = process.env.PORT || 4022;
 
 let home = "./views/home.html"
@@ -10,26 +11,27 @@ let productDetail = "./views/productDetail.html"
 let signUp = "./views/signUp.html"
 let signIn = "./views/signIn.html"
 
-
+app.set('view engine', 'ejs');
 app.use(express.static(publicPath))
+
 app.listen(port, () => {
     console.log("Servidor prueba Gamestore corriendo en puerto 4022")
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, home))
+    res.render('home')
 })
 app.get("/home", (req, res) => {
-    res.sendFile(path.resolve(__dirname, home))
+    res.render('home')
 })
 app.get("/shopping-cart", (req, res) => {
-    res.sendFile(path.resolve(__dirname, shopcart))
+    res.render('shopcart')
 })
 app.get("/product-detail", (req, res) => {
-    res.sendFile(path.resolve(__dirname, productDetail))
+    res.render('productDetail')
 })
 app.get("/sign-up", (req, res) => {
-    res.sendFile(path.resolve(__dirname, signUp))
+    res.render('signUp')
 })
 app.get("/sign-in", (req, res) => {
-    res.sendFile(path.resolve(__dirname, signIn))
+    res.render('signIn')
 })
