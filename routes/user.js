e2a0abed87d52eb7a0bfcfe883f7416db9ae13e8
esteban18498 <esteban18498@gmail.com>
2021-11-04 20:36:11 -0300

    [FIX] cartController -> userController

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const cartController = require ('../controllers/user-controller.js')
+const userController = require ('../controllers/user-controller.js')
 
 router.get('/log-in', userController.logIn);
 router.get('/sign-up', userController.signUp);
