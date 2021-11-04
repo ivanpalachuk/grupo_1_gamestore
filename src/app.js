9566434914c6c7a0ed80b6dad79280e52b2293d1
Mcsand22 <mabernal94@gmail.com>
2021-11-04 19:10:35 -0300

    MOVE carpetas

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const publicPath = path.resolve(__dirname, "./public");
+const port = process.env.PORT || 4022;
+
+let home = "./views/home.html"
+let shopcart = "./views/shoppingcart.html"
+let productDetail = "./views/productDetail.html"
+let signUp = "./views/signUp.html"
+let signIn = "./views/signIn.html"
+
+
+app.use(express.static(publicPath))
+app.listen(port, () => {
+    console.log("Servidor prueba Gamestore corriendo en puerto 4022")
+})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, home))
+})
+app.get("/home", (req, res) => {
+    res.sendFile(path.resolve(__dirname, home))
+})
+app.get("/shopping-cart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, shopcart))
+})
+app.get("/product-detail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, productDetail))
+})
+app.get("/sign-up", (req, res) => {
+    res.sendFile(path.resolve(__dirname, signUp))
+})
+app.get("/sign-in", (req, res) => {
+    res.sendFile(path.resolve(__dirname, signIn))
+})
