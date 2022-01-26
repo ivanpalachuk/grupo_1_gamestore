dbe5264be01770ca09d8cc4c65bca60f01246823
ivanpalachuk <ivanzar@hotmail.com>
2022-01-26 20:02:27 -0300

    Add detail controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -14,8 +14,25 @@ const productController = {
             res.render('products', { products: p });
         })
     },
-    PaginaCrear: (req, res) => {
-        res.render('new-game');
+    PaginaCrear: async(req, res) => {
+        let Edades = await db.Edad.findAll()
+        let Plataformas = await db.Plataforma.findAll()
+        Plataformas = Plataformas.map((Plat) => {
+            return Plat.nombre
+        })
+        let Dificultades = await db.Dificultad.findAll()
+        let Categorias = await db.Categoria.findAll()
+        Categorias = Categorias.map((Cat) => {
+            return Cat.nombre
+        })
+        res.render('new-game', {
+            Edades,
+            Plataformas,
+            Dificultades,
+            Categorias
+        });
+
+
     },
     Detail: (req, res) => {
 
@@ -46,12 +63,12 @@ const productController = {
 
     },
 
-    PaginaEdit: async (req, res) => {
+    PaginaEdit: async(req, res) => {
 
         let productId = req.params.id;
 
         let Edades = await db.Edad.findAll()
-        
+
         let Plataformas = await db.Plataforma.findAll()
         Plataformas = Plataformas.map((Plat) => {
             return Plat.nombre
@@ -62,59 +79,54 @@ const productController = {
             return Cat.nombre
         })
 
-        Product.findByPk(productId,
-            {
-                include: ['Dificultad', 'Edad', 'ImagenPrincipal', 'ImagenSecundaria', 'Plataformas', 'Categorias']
-            }).then((p) => {
-                console.log("-----------------dataModeling----------------------")
-                p.Plataformas = p.Plataformas.map((Plat) => {
-                    return Plat.nombre
-                })
-                p.Categorias = p.Categorias.map((Cat) => {
-                    return Cat.nombre
-                })
-                console.log(p);
-                console.log("---------------------------------------")
-                res.render('edit-game', {
-                    product: p,
-                    Edades,
-                    Plataformas,
-                    Dificultades,
-                    Categorias
-                })
-            }).catch((e) => {
-                console.log("ERROR")
-                console.log(e)
+        Product.findByPk(productId, {
+            include: ['Dificultad', 'Edad', 'ImagenPrincipal', 'ImagenSecundaria', 'Plataformas', 'Categorias']
+        }).then((p) => {
+            console.log("-----------------dataModeling----------------------")
+            p.Plataformas = p.Plataformas.map((Plat) => {
+                return Plat.nombre
             })
+            p.Categorias = p.Categorias.map((Cat) => {
+                return Cat.nombre
+            })
+            console.log(p);
+            console.log("---------------------------------------")
+            res.render('edit-game', {
+                product: p,
+                Edades,
+                Plataformas,
+                Dificultades,
+                Categorias
+            })
+        }).catch((e) => {
+            console.log("ERROR")
+            console.log(e)
+        })
 
 
     },
     Crear: (req, res) => {
 
-        console.log(req.body.dificult)
         productoNuevo = {
             "titulo": req.body.titulo,
             "precio": req.body.price,
             "descuento": req.body.discount,
-            "Dificultad": req.body.Dificultad,
-            "Edad": req.body.Edad,
+            "resumen": req.body.resumen,
+            "idDificultad"
+            "idEdad": req.body.Edad,
             "plataforma": req.body.plataforma,
-            "developer": req.body.developer,
-
             "datosTecnicos": req.body.datos_Tecnicos,
             "requisitos": req.body.requisitos,
-
-            "web": req.body.web,
-            "category": req.body.categoryOption,
-            "resumen": req.body.resumen,
-
             "legal": req.body.legal,
-            //"idImagenPrincipal": (req.files['photoGameV'] ? req.files['photoGameV'][0].filename : 0),
-            //"idImagenSecundaria": (req.files['photoGame'] ? req.files['photoGame'][0].filename : 0),
+            "idImagenPrincipal": (req.files['photoGameV'] ? req.files['photoGameV'][0].filename : 4),
+            "idImagenSecundaria": (req.files['photoGame'] ? req.files['photoGame'][0].filename : 5),
 
         }
-
-        Product.create(productoNuevo)
+        console.log(productoNuevo)
+        Product.create(productoNuevo).catch((e) => {
+            console.log("ERROR")
+            console.log(e)
+        })
 
 
 
