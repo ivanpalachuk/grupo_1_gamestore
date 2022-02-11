6fa97fc3cd711e3a65fe82abb80a8373574ddc97
Mcsand22 <mabernal94@gmail.com>
2022-02-10 21:44:06 -0300

    CREATE extensiones fotos permitidas

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -1,7 +1,8 @@
 const {body} = require('express-validator');
 
 module.exports = [
-    body('nombre').notEmpty().withMessage('*Este campo debe estar completo'),
+    body('nombre').notEmpty().withMessage('*Este campo debe estar completo').bail()
+                  .isLength({min: 2}).withMessage('*Este campo debe tener min 2 caracteres'),
     body('apellido').notEmpty().withMessage('*Este campo debe estar completo'),
     body('dni').notEmpty().withMessage('*Este campo debe estar completo').bail()
         .isNumeric().withMessage('*Este campo debe ser numérico'),
@@ -11,8 +12,8 @@ module.exports = [
         .isEmail().withMessage('*Este campo debe ser un correo electrónico'),
 
         // No funciona - revisar
-     //body('clave').notEmpty().withMessage('*Este campo debe estar completo')//.bail()
-    //              .isLength({min: 8}).withMessage('*Este campo debe contener min 8 caracteres'),
+    body('clave').notEmpty().withMessage('*Este campo debe estar completo').bail()
+                  .isLength({min: 8}).withMessage('*Este campo debe contener min 8 caracteres'),
     // body('passwordConfirmation').notEmpty().withMessage('*Este campo debe estar completo').bail()
     //     .custom((value, {req}) => {
     //     if (value !== req.body.password) {
@@ -28,8 +29,15 @@ module.exports = [
     body('tcno').notEmpty().withMessage('*Debes aceptar los términos y condiciones'),
     body('avatar').custom((value, {req}) => {
     let file = req.file;
+    let acceptedExtensions = ['.jpg', '.png', '.gif', '.jpeg'];
+
     if (!file){
         throw new Error ('*Tienes que subir una imagen');
+    } else{
+        let fileExtension = path.extname(file.originalname);
+        if(acceptedExtensions.includes(fileExtension)){
+            throw new Error ('*Las extensiones permitidas son .JPG, .JPEG, .PNG y .GIF');
+        }
     }
     return true;
     })
