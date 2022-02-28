f2bc42ebac922c6522d27e592e439c02118b49e0
esteban18498 <esteban18498@gmail.com>
2022-02-28 19:24:40 -0300

    add api products

--- src/controllers/Apis/api-product-controller.js
+++ src/controllers/Apis/api-product-controller.js
@@ -4,30 +4,83 @@ const Product = db.Producto
 
 
 const apiProductController = {
-    list: (req,res) => {
+    list: (req, res) => {
         Product.findAll({
             include: ['Dificultad', 'Edad', 'ImagenPrincipal', 'ImagenSecundaria', 'Plataformas', 'Categorias']
         }).then((products) => {
-            products.forEach(p => {
-                if (p.Plataformas) {
-                    p.Plataformas = p.Plataformas.map((Plat) => {
-                        return Plat.nombre
-                    })
-                }
-                if (p.Categorias) {
-                    p.Categorias = p.Categorias.map((Cat) => {
+
+            let productsList = products.map((p) => {
+                let product = {
+                    "id": p.id,
+                    "name": p.titulo,
+                    "description": p.resumen,
+                    "categories": p.Categorias.map((Cat) => {
                         return Cat.nombre
-                    })
+                    }),
+                    "detail": "http://localhost:4022/api/product/" + p.id
                 }
-                if (p.ImagenPrincipal) {
-                    p.ImagenPrincipal = p.ImagenPrincipal.ruta
-                }
-                if (p.ImagenSecundaria) {
-                    p.ImagenSecundaria = p.ImagenSecundaria.ruta
+                return product
+
+            })
+
+            db.Categoria.findAll({ include: ['Productos'] }).then((categorias) => {
+
+                categorias = categorias.reduce((acc, curr) => (acc[curr.nombre] = curr.Productos.length, acc), {});
+                //console.log(categorias)
+
+                let response = {
+                    count: productsList.length,
+                    countByCategory: categorias,
+                    products: productsList
                 }
 
-            });
-            return res.send(products);
+                return res.send(response);
+            })
+
+
+
+        })
+    },
+    detail: (req, res) => {
+        let productId = req.params.id;
+
+        Product.findByPk(productId, {
+            include: ['Dificultad', 'Edad', 'ImagenPrincipal', 'ImagenSecundaria', 'Plataformas', 'Categorias']
+        }).then((p) => {
+
+
+            if (p.dataValues.ImagenPrincipal) {
+                p.dataValues.ImagenPrincipal = "http://localhost:4022/images/productImages" + p.dataValues.ImagenPrincipal.ruta
+            }
+            if (p.dataValues.ImagenSecundaria) {
+                p.dataValues.ImagenSecundaria = "http://localhost:4022/images/productImages" + p.dataValues.ImagenSecundaria.ruta
+            }
+            if (p.dataValues.Plataformas) {
+                p.dataValues.Plataformas = p.dataValues.Plataformas.map((Plat) => {
+                    return Plat.nombre
+                })
+            }
+            if (p.dataValues.Categorias) {
+                p.dataValues.Categorias = p.dataValues.Categorias.map((Cat) => {
+                    return Cat.nombre
+                })
+            }
+            if (p.dataValues.Edad) {
+                p.dataValues.Edad = p.dataValues.Edad.nombre
+
+            }
+            if (p.dataValues.Dificultad) {
+                p.dataValues.Dificultad = p.dataValues.Dificultad.nombre
+
+            }
+
+            p = p.dataValues
+
+            console.log(p)
+
+            res.send(p)
+
+
         })
     }
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,6 +4,8 @@ const productController = require ('../controllers/Apis/api-product-controller')
 const userController = require ('../controllers/Apis/api-user-controller')
 
 router.get('/product', productController.list);
+router.get('/product/:id', productController.detail);
+
 
 router.get('/user', userController.list);
 router.get('/user/:id', userController.detail);
